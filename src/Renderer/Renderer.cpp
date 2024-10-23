7e7376fbc5e42b2a8967577db1d51075e498487e
L1bra <L1bradv86@gmail.com>
2024-10-23 23:50:10 +1000

    index buffer

--- src/Renderer/Renderer.cpp
+++ src/Renderer/Renderer.cpp
@@ -12,21 +12,29 @@ Renderer::~Renderer()
 
 void Renderer::InitData()
 {
-    float vertices[] = 
-    {
-        // pos
-        -0.5f, -0.5f, 0.0f,
-         0.5f, -0.5f, 0.0f,
-         0.0f,  0.5f, 0.0f
+    float vertices[] = {
+         0.5f,  0.5f, 0.0f,  // top right
+         0.5f, -0.5f, 0.0f,  // bottom right
+        -0.5f, -0.5f, 0.0f,  // bottom left
+        -0.5f,  0.5f, 0.0f   // top left 
     };
-    
+
+    unsigned int indices[] = {
+        0, 1, 3,   // first triangle
+        1, 2, 3    // second triangle
+    };
+
     m_VAO.bind();
 
     m_VBO.reset(VertexBuffer::Create(vertices, sizeof(vertices)));
     m_VBO->Bind();
 
+    m_IBO.reset(IndexBuffer::Create(indices, 6));
+    m_IBO->Bind();
+
     m_VBO->Unbind();
     m_VAO.unbind();
+    m_IBO->Unbind();
 
     // default shader
     m_Shader = Global::ResourceManager::GetInstance().LoadShader("src/Shaders/vertex.vs", "src/Shaders/fragment.fs", nullptr, "sprite");
@@ -36,6 +44,7 @@ void Renderer::InitData()
 void Renderer::DrawSprite(glm::vec2 position, glm::vec2 size, float rotate, glm::vec3 color /*, Shader& shader*/)
 {
     m_VAO.bind();
-    glDrawArrays(GL_TRIANGLES, 0, 6);
+    //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE); // Wireframe mode
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
     m_VAO.unbind();
 }
